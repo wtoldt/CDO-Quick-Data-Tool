20845aaa73e20423f53467a41eaea3e10f4b471a
Wes Oldt <Wes Oldt>
2017-04-06 22:28:58 -0400

    Should have happened long ago

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,118 +1,118 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { CdoService } from './cdo-service.service';
 import { CdoResponse } from './cdo-response.class';
 import { CdoStationRequest } from './cdo-stations-request.class';
-import { CdoStation  } from './cdo-station.class';
+import { CdoStation } from './cdo-station.class';
 
 declare var L: any;
 declare var $: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  cdoResponse = new CdoResponse();
-  map: any;
-  markerLayer: any = L.layerGroup();
-  date = "";
-  loading = false;
+export class AppComponent implements OnInit {
+	cdoResponse = new CdoResponse();
+	map: any;
+	markerLayer: any = L.layerGroup();
+	date = "";
+	loading = false;
 
-  constructor (private cdoService: CdoService) { }
+	constructor(private cdoService: CdoService) { }
 
-  ngOnInit() {
-    this.cdoService.getDatasetMaxDate('GHCND').subscribe( date => {
-      this.date = date;
-    });
-    this.onResize();
+	ngOnInit() {
+		this.cdoService.getDatasetMaxDate('GHCND').subscribe(date => {
+			this.date = date;
+		});
+		this.onResize();
 
-    this.map = L.map('map').setView([43.61, -98.50], 4);
+		this.map = L.map('map').setView([43.61, -98.50], 4);
 
-    var Esri_WorldStreetMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-	    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
-    }).addTo(this.map);
+		const Esri_WorldStreetMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+			attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+		}).addTo(this.map);
 
-    this.markerLayer.addTo(this.map);
+		this.markerLayer.addTo(this.map);
 
-    // geocode search
-    let searchControl = L.esri.Geocoding.geosearch().addTo(this.map),
-        results = L.layerGroup().addTo(this.map);
+		// geocode search
+		let searchControl = L.esri.Geocoding.geosearch().addTo(this.map),
+			results = L.layerGroup().addTo(this.map);
 
-    // idle event doesn't exist, make due with what we got
-    this.map.on('load moveend', e => {
+		// idle event doesn't exist, make due with what we got
+		this.map.on('load moveend', e => {
 
-      let stationRequest = new CdoStationRequest(),
-          bounds = this.map.getBounds();
-      stationRequest.datasetid = 'GHCND';
-      stationRequest.extent = 
-        `${bounds.getSouth()},${bounds.getWest()},${bounds.getNorth()},${bounds.getEast()}`;
-      stationRequest.startdate = '2017-01-01T00:00:00';
-      stationRequest.enddate = '2017-01-01T23:59:59';
-      
-      this.loading = true;
-      $('.leaflet-container').addClass('loading');
-      this.cdoService.getStations(stationRequest).subscribe( 
-        (stations: CdoStation[]) => {
-          this.refreshStationMarkers(stations);
-        },
-        error => {console.log(error)},
-        () => {
-          this.loading = false;
-          $('.leaflet-container').removeClass('loading');
-        }
-      );
-    });
-  }
+			let stationRequest = new CdoStationRequest(),
+				bounds = this.map.getBounds();
+			stationRequest.datasetid = 'GHCND';
+			stationRequest.extent =
+				`${bounds.getSouth()},${bounds.getWest()},${bounds.getNorth()},${bounds.getEast()}`;
+			stationRequest.startdate = '2017-01-01T00:00:00';
+			stationRequest.enddate = '2017-01-01T23:59:59';
 
-  onResize() {
-    let windowHeight: number,
-        navHeight: number;
+			this.loading = true;
+			$('.leaflet-container').addClass('loading');
+			this.cdoService.getStations(stationRequest).subscribe(
+				(stations: CdoStation[]) => {
+					this.refreshStationMarkers(stations);
+				},
+				error => { console.log(error) },
+				() => {
+					this.loading = false;
+					$('.leaflet-container').removeClass('loading');
+				}
+			);
+		});
+	}
 
-    windowHeight = window.innerHeight;
-    navHeight = window.document.getElementById('nav').clientHeight;
+	onResize() {
+		let windowHeight: number,
+			navHeight: number;
 
-     window.document.getElementById('map').style.height =
-        (windowHeight - navHeight) + 'px';
-    
-  }
+		windowHeight = window.innerHeight;
+		navHeight = window.document.getElementById('nav').clientHeight;
 
-  refreshStationMarkers(stations: CdoStation[]) {
-    this.markerLayer.clearLayers();
-    stations.forEach( (station: CdoStation) => {
-      this.markerLayer.addLayer(
-        L.marker([station.latitude, station.longitude], {icon: L.AwesomeMarkers.icon({icon: 'record', markerColor: 'red', prefix: 'glyphicon'}) }).bindPopup(this.makePopupContent(station))
-      );
-    });
-  }
+		window.document.getElementById('map').style.height =
+			(windowHeight - navHeight) + 'px';
 
-  makePopupContent(station: CdoStation): string {
-    let content = `
-      <div class="container-fluid">
-        <div class="row">
+	}
 
-          <div class="col-md-12">
-            <h3>
-              ${station.name} <br />
-              <small>${station.id}</small>
-            </h3>
-          </div>
+	refreshStationMarkers(stations: CdoStation[]) {
+		this.markerLayer.clearLayers();
+		stations.forEach((station: CdoStation) => {
+			this.markerLayer.addLayer(
+				L.marker(
+					[station.latitude, station.longitude],
+					{ icon: L.AwesomeMarkers.icon({ icon: 'record', markerColor: 'red', prefix: 'glyphicon' }) }).bindPopup(this.makePopupContent(station))
+			);
+		});
+	}
 
-          <div class="col-md-12" style="text-align:center">
-            <h1><span class="glyphicon glyphicon-cd spin" aria-hidden="true"></span></h1>
-          </div>
+	makePopupContent(station: CdoStation): string {
+		let content = `
+			<div class="container-fluid">
+				<div class="row">
 
-        </div>
-      </div>
-    `;
+					<div class="col-md-12">
+						<h3>
+							${station.name} <br />
+							<small>${station.id}</small>
+						</h3>
+					</div>
 
-    return content;
-  }
+					<div class="col-md-12" style="text-align:center">
+						<h1><span class="glyphicon glyphicon-cd spin" aria-hidden="true"></span></h1>
+					</div>
 
+				</div>
+			</div>
+		`;
 
+		return content;
+	}
 
-  onDateUpdate(date) {
-    console.log(date);
-  }
+	onDateUpdate(date) {
+		console.log(date);
+	}
 }
--- src/app/cdo-service.service.ts
+++ src/app/cdo-service.service.ts
@@ -5,75 +5,75 @@ import { Observable } from 'rxjs';
 
 import { CdoResponse } from './cdo-response.class';
 import { CdoStationRequest } from './cdo-stations-request.class';
-import { CdoStation  } from './cdo-station.class';
+import { CdoStation } from './cdo-station.class';
 
 @Injectable()
 export class CdoService {
-  baseUrl = 'https://www.ncdc.noaa.gov/cdo-web/api/v2';
-  token = 'IdNEXjwZWEjvmkHMrRgJLNfxijCdyzFC';
+	baseUrl = 'https://www.ncdc.noaa.gov/cdo-web/api/v2';
+	token = 'IdNEXjwZWEjvmkHMrRgJLNfxijCdyzFC';
 
-  constructor(private http: Http) { }
+	constructor(private http: Http) { }
 
-  getStations(stationRequest: CdoStationRequest): Observable<CdoStation[]> {
-    let headers = new Headers(),    
-        params: URLSearchParams = CdoStationRequest.toSearchParams(stationRequest);
+	getStations(stationRequest: CdoStationRequest): Observable<CdoStation[]> {
+		let headers = new Headers(),
+			params: URLSearchParams = CdoStationRequest.toSearchParams(stationRequest);
 
-    headers.set('token', this.token);
+		headers.set('token', this.token);
 
-    return this.http.get(`${this.baseUrl}/stations`, {'search': params, 'headers': headers})
-      .map( response => this.handleStationsResponse(response))
-      .catch( error => this.handleError(error));
-  }
+		return this.http.get(`${this.baseUrl}/stations`, { 'search': params, 'headers': headers })
+			.map(response => this.handleStationsResponse(response))
+			.catch(error => this.handleError(error));
+	}
 
-  getData(): Observable<CdoResponse> {
+	getData(): Observable<CdoResponse> {
 
-    let headers = new Headers();
-    headers.set('token', this.token);
+		let headers = new Headers();
+		headers.set('token', this.token);
 
-    let url = `${this.baseUrl}/data?datasetid=GHCND&staitonid=GHCND:US1NCBC0051&startdate=2017-03-01&enddate=2017-03-02&units=metric`
+		let url = `${this.baseUrl}/data?datasetid=GHCND&staitonid=GHCND:US1NCBC0051&startdate=2017-03-01&enddate=2017-03-02&units=metric`
 
-   
-    return this.http.get(url, {'headers': headers})
-      .map( response => this.handleDataResponse(response) )
-      .catch( error => this.handleError(error) );
-  }
 
-  getDatasetMaxDate(datasetid: string): Observable<string> {
-    let headers = new Headers();
-    headers.set('token', this.token);
+		return this.http.get(url, { 'headers': headers })
+			.map(response => this.handleDataResponse(response))
+			.catch(error => this.handleError(error));
+	}
 
-    let url = `${this.baseUrl}/datasets/${datasetid}`;
+	getDatasetMaxDate(datasetid: string): Observable<string> {
+		let headers = new Headers();
+		headers.set('token', this.token);
 
-    return this.http.get(url, {'headers': headers})
-      .map( response => response.json().maxdate)
-      .catch( error => this.handleError(error));
-  }
+		let url = `${this.baseUrl}/datasets/${datasetid}`;
 
-  private handleStationsResponse(response: Response): CdoStation[] {
-    let body = response.json(),
-        stations: CdoStation[] = [];
-    
-    body.results.forEach( stationJson => {
-      stations.push(CdoStation.fromJson(stationJson));
-    });
-    
-    return stations;
-  }
+		return this.http.get(url, { 'headers': headers })
+			.map(response => response.json().maxdate)
+			.catch(error => this.handleError(error));
+	}
 
-  private handleDataResponse(response: Response): CdoResponse {
-  
-    let body = response.json(),
-        cdoResponse = new CdoResponse();
+	private handleStationsResponse(response: Response): CdoStation[] {
+		let body = response.json(),
+			stations: CdoStation[] = [];
 
-        cdoResponse.metadata = body.metadata;
-        cdoResponse.results = body.results;
+		body.results.forEach(stationJson => {
+			stations.push(CdoStation.fromJson(stationJson));
+		});
 
-    return cdoResponse;
-  }
+		return stations;
+	}
 
-  private handleError(error: Response | any) {
-  
-    return Observable.throw('poop');
-  }
+	private handleDataResponse(response: Response): CdoResponse {
+
+		let body = response.json(),
+			cdoResponse = new CdoResponse();
+
+		cdoResponse.metadata = body.metadata;
+		cdoResponse.results = body.results;
+
+		return cdoResponse;
+	}
+
+	private handleError(error: Response | any) {
+
+		return Observable.throw('poop');
+	}
 
 }
